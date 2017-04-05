6e1340de9cf0a1e58c56c8979657070032b20fa3
Hyojin <minihyojin@gmail.com>
2017-04-05 18:25:17 -0500

    removed join table migration

--- db/migrate/20170405194927_create_join_table_users_umbrellas.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersUmbrellas < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :users, :umbrellas do |t|
-      t.index :user_id
-      t.index :umbrella_id
-    end
-  end
-end
